1b6f99eed84dd0aecc1bd63ae117872db3453b8a
Éricles Miller <59926475+Ericles-Miller@users.noreply.github.com>
2025-11-10 19:04:11 -0300

    refactor: reorganize Calculator component and update imports

---
+++ src/components/Calculator.tsx
@@ -0,0 +1,18 @@
+import Numbers from "./Numbers";
+import Operator from "./Operators";
+
+function Calculator() {
+  return (
+    <div className="w-393 h-196 bg-gray-800 rounded-lg shadow-xl p-6">
+      <h2 className="text-2xl font-bold text-center mb-4 text-white">
+        Calculadora
+      </h2>
+      <div className="flex gap-4">
+        <Numbers />
+        <Operator />
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
